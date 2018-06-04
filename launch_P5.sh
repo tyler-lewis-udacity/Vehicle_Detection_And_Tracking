9a93664ed07f6dd8d99f0f871df90b241862b612
Tyler Lewis <tyler.lewis.udacity@gmail.com>
2018-06-04 13:13:50 -0500

    Added quick launch .sh script.

--- launch_P5.sh
+++ launch_P5.sh
@@ -3,7 +3,7 @@
 # This script will activate the P5 conda environment and launch
 # the P5_advanced_lane_finding.ipynb jupyter notebook:
 
-cd "/home/ty/Udacity/T1/P5_vehicle_detection_and_tracking"
-source "/home/ty/anaconda3/bin/activate" P5
+cd "/Users/ty/Udacity/T1/P5_vehicle_detection_and_tracking"
+source "/Users/ty/anaconda3/bin/activate" P5
 jupyter notebook P5_vehicle_detection_and_tracking.ipynb
 
